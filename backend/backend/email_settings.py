9f45e56871fe53dbb9af08048a347c96cad9e417
PalTrivedi <palvtrivedi@gmail.com>
2025-09-02 09:33:33 +0530

    commit

--- backend/backend/email_settings.py
+++ backend/backend/email_settings.py
@@ -1,9 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 # Email Configuration
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'officialplanetpulse@gmail.com'
-EMAIL_HOST_PASSWORD = 'jbvt hvjb uvqz uzmx'  # App password for Gmail
-DEFAULT_FROM_EMAIL = 'PlanetPulse <officialplanetpulse@gmail.com>'
-SERVER_EMAIL = 'officialplanetpulse@gmail.com'
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True').lower() == 'true'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', f'{EMAIL_HOST_USER}')
+SERVER_EMAIL = os.getenv('SERVER_EMAIL', EMAIL_HOST_USER)
